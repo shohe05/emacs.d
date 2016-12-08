163af30ae61f79743d71d2db1a3a6b4a2ae71920
Syouhei Tanaka <shou.tanak@gmail.com>
2016-12-09 02:45:14 +0900

    git gutter settings

--- init.el
+++ init.el
@@ -117,18 +117,3 @@
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>")) ;; use C-h as delete key
 (global-set-key (kbd "C-x C-g") 'goto-line)
 (global-set-key (kbd "C-u") 'undo)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(git-gutter:added-sign "++")
- '(git-gutter:deleted-sign "--")
- '(git-gutter:modified-sign "  ")
- '(package-selected-packages (quote (php-completion))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- init/init-git-gutter.el
+++ init/init-git-gutter.el
@@ -1,10 +1,17 @@
 (global-git-gutter-mode t)
 (git-gutter:linum-setup)
-(custom-set-variables
- '(git-gutter:modified-sign "  ") ;; two space
- '(git-gutter:added-sign "++")    ;; multiple character is OK
- '(git-gutter:deleted-sign "--"))
 
-(set-face-background 'git-gutter:modified "purple") ;; background color
-(set-face-foreground 'git-gutter:added "green")
-(set-face-foreground 'git-gutter:deleted "red")
+;; (custom-set-variables
+;;  '(git-gutter:modified-sign "=") ;; two space
+;;  '(git-gutter:added-sign "+")    ;; multiple character is OK
+;;  '(git-gutter:deleted-sign "-"))
+
+;; (custom-set-variables
+;;  '(git-gutter:modified-sign "☁")
+;;  '(git-gutter:added-sign "☀")
+;;  '(git-gutter:deleted-sign "☂"))
+ ;; '(git-gutter:window-width 2)
+
+;; (set-face-background 'git-gutter:modified "purple") ;; background color
+;; (set-face-foreground 'git-gutter:added "green")
+;; (set-face-foreground 'git-gutter:deleted "red")
