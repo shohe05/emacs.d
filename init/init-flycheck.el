a492c1cef149f0c2987a28f107d166b8a3c41544
Syouhei Tanaka <shou.tanak@gmail.com>
2016-12-09 18:42:33 +0900

    trigger js2-jsx-mode on .jsx file & flycheck for jsx

--- init/init-flycheck.el
+++ init/init-flycheck.el
@@ -3,3 +3,7 @@
 (define-key global-map (kbd "C-c n") 'flycheck-next-error)
 (define-key global-map (kbd "C-c p") 'flycheck-previous-error)
 (define-key global-map (kbd "C-c l") 'flycheck-list-errors)
+
+;; jsx
+(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+(add-hook 'js2-jsx-mode-hook 'flycheck-mode)
