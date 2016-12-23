95f87fbeb9997ebfd94baa7b4eec79ab05e50d65
Syouhei Tanaka <shou.tanak@gmail.com>
2016-12-23 18:07:24 +0900

    update expand-region keybind

--- init/init-expand-region.el
+++ init/init-expand-region.el
@@ -1,3 +1,3 @@
-(global-set-key (kbd "C-o") 'er/expand-region)
-(global-set-key (kbd "M-o") 'er/contract-region) ;; リージョンを狭める
+(global-set-key (kbd "M-o") 'er/expand-region)
+(global-set-key (kbd "C-o") 'er/contract-region) ;; リージョンを狭める
 (transient-mark-mode t)
