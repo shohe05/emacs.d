94fdf9ed33b911e67c956af37dee21501706f5ab
Syouhei Tanaka <shou.tanak@gmail.com>
2017-06-20 19:35:59 +0900

    show kill ring

--- init/init-helm.el
+++ init/init-helm.el
@@ -10,5 +10,8 @@
 ;; C-x b を helm-buffer-listに
 (define-key global-map (kbd "C-j C-b") 'helm-buffers-list)
 
+;; show kill-ring
+(define-key global-map (kbd "M-r") 'helm-show-kill-ring)
+
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action) ;; タブでファイル名/ディレクトリ名を補完
 (helm-mode 1) ;; helm起動
